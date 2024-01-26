4d4c9bbd24e5bea7a728bfbced11ba09fd81d297
Pushkraj Lanjekar <pushkraj.lanjekar@gmail.com>
2024-01-26 20:29:49 +0530

    Initial Commit

---
+++ Sources/CustomToast/PLToast.swift
@@ -0,0 +1,84 @@
+//
+//  PLToast.swift
+//
+//
+//  Created by Pushkraj Lanjekar on 26/01/24.
+//
+
+import Foundation
+import SwiftUI
+
+enum PLToastStyle {
+    case error
+    case warning
+    case success
+    case info
+}
+
+extension PLToastStyle {
+    @available(iOS 13.0, *)
+    var themeColor: Color {
+        switch self {
+        case .error: return Color.red
+        case .warning: return Color.orange
+        case .info: return Color.blue
+        case .success: return Color.green
+        }
+    }
+    
+    var iconFileName: String {
+        switch self {
+        case .info: return "info.circle.fill"
+        case .warning: return "exclamationmark.triangle.fill"
+        case .success: return "checkmark.circle.fill"
+        case .error: return "xmark.circle.fill"
+        }
+    }
+}
+
+public struct PLToastView: View {
+    var type: PLToastStyle
+    var title: String
+    var message: String
+    var onCancelTapped: (() -> Void)
+    @available(iOS 13.0.0, *)
+    public var body: some View {
+        VStack(alignment: .leading) {
+            HStack(alignment: .top) {
+                Image(systemName: type.iconFileName)
+                    .foregroundColor(type.themeColor)
+                
+                VStack(alignment: .leading) {
+                    Text(title)
+                        .font(.system(size: 14, weight: .semibold))
+                    
+                    Text(message)
+                        .font(.system(size: 12))
+                        .foregroundColor(Color.black.opacity(0.6))
+                }
+                
+                Spacer(minLength: 10)
+                
+                Button {
+                    onCancelTapped()
+                } label: {
+                    Image(systemName: "xmark")
+                        .foregroundColor(Color.black)
+                }
+            }
+            .padding()
+        }
+        .background(Color.white)
+        .overlay(
+            Rectangle()
+                .fill(type.themeColor)
+                .frame(width: 6)
+                .clipped()
+            , alignment: .leading
+        )
+        .frame(minWidth: 0, maxWidth: .infinity)
+        .cornerRadius(8)
+        .shadow(color: Color.black.opacity(0.25), radius: 4, x: 0, y: 1)
+        .padding(.horizontal, 16)
+    }
+}
---
+++ Tests/CustomToastTests/CustomToastTests.swift
@@ -0,0 +1,12 @@
+import XCTest
+@testable import CustomToast
+
+final class CustomToastTests: XCTestCase {
+    func testExample() throws {
+        // XCTest Documentation
+        // https://developer.apple.com/documentation/xctest
+
+        // Defining Test Cases and Test Methods
+        // https://developer.apple.com/documentation/xctest/defining_test_cases_and_test_methods
+    }
+}
